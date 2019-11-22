0165760a4229cc8e030e3121488c476f0545cc17
coio <ioannou.connie@gmail.com>
2019-11-22 11:06:17 +0100

    dfg
    dfg

---
+++ src/test/resources/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		<dependency>
+		    <groupId>org.codehaus.jettison</groupId>
+		    <artifactId>jettison</artifactId>
+		    <version>1.2</version>
+		</dependency>
+		
+	}
+}
+
