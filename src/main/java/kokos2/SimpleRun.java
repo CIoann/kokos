8b2eae9c2f469ea5fcd35cf6e0288b9987c87515
coio <ioannou.connie@gmail.com>
2019-11-25 12:07:12 +0100

    new

---
+++ src/main/java/kokos2/SimpleRun.java
@@ -0,0 +1,12 @@
+package kokos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRun {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRun.class, args);
+	}
+}
